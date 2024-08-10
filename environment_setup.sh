5cfaf3d7464a7d79a25d01618924e8e5168e8559
root <root@ip-10-0-35-114.ap-southeast-1.compute.internal>
2024-08-10 12:07:23 +0000

    Added script for environment setup

---
+++ environment_setup.sh
@@ -0,0 +1,43 @@
+﻿
+
+
+
+#!/bin/bash
+
+sudo root
+
+chmod 644 /usr/lib/sudo/sudoers.so
+
+chown -R root /usr/lib/sudo
+
+pkexec su
+
+sudo apt update
+
+sudo apt-get install tesseract-ocr
+
+sudo apt install python3 python3-pip
+
+pip3 install jupyterlab
+
+jupyter lab 
+
+pip install --upgrade six ipython
+
+pip uninstall six
+
+pip install six
+
+pip install pandas
+ 
+pip install numpy
+
+pip install pytesseract
+
+
+
+
+
+
+
+
